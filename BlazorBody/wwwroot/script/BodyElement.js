1357b8c7b82c4576ab61a0ff48a717e657d67250
Benjamin Charlton <benjaminjcharlton@gmail.com>
2020-08-24 18:43:31 +0400

    Add project files.

---
+++ BlazorBody/wwwroot/script/BodyElement.js
@@ -0,0 +1,42 @@
+﻿var getBodyElement = function() {
+    return document.getElementsByTagName("body")[0];
+}
+
+var setCssClassOfBody = function (cssClass) {
+    getBodyElement().className = cssClass;
+}
+
+var addCssClassToBody = function (cssClass) {
+    var body = getBodyElement();
+
+    if (!body.classList.contains(cssClass)) {
+        body.classList.add(cssClass);
+    }
+}
+
+var removeCssClassFromBody = function (cssClass) {
+    var body = getBodyElement();
+    if (body.classList.contains(cssClass))
+        body.classList.remove(cssClass);
+}
+
+var toggleCssClassOfBody = function (cssClass) {
+    var body = getBodyElement();
+
+    if (body.classList.contains(cssClass)) {
+        removeCssClassFromBody(cssClass);
+    }
+    else {
+        addCssClassToBody(cssClass);
+    }
+}
+
+var setLanguageOfBody = function (language) {
+    var body = getBodyElement();
+    body.lang = language;
+}
+
+var setTextDirectionOfBody = function (direction) {
+    var body = getBodyElement();
+    body.dir = direction;
+}
